da54ff2b2e8703bdbb9baec31d1e15a7f7e6f4fc
lizhou <lizhou1021@qq.com>
2021-10-08 15:16:11 +0800

    改为ESM结构

--- convert.js
+++ convert.js
@@ -1,4 +1,13 @@
 // 转换commandJs 2 ESM
-
+import $ from 'gogocode';
 import fs from 'fs'
-import path from 'path'
+
+fs.readdir('./lib',(err,files)=>{
+    console.log('files: ', files);
+    for(const file of files){
+        const code=fs.readFileSync("./lib/"+file,{encoding:"utf-8"});
+        const AST = $(code);
+        AST.replace('const $_$1 = require($_$2);\r\n', 'import $_$1 from $_$2;\r\n');
+        fs.writeFileSync('./lib/'+file,AST.generate());
+    }
+})
